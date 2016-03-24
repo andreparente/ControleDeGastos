5a5bc4e64fd85bff12bdd7c794ca993bdce54910
Caio Valente <caio-valente@hotmail.com>
2016-03-24 15:47:00 -0300

    alteracoes nas classes base do projeto

--- ControleDeGastos/Classes.swift
+++ ControleDeGastos/Classes.swift
@@ -10,41 +10,90 @@ import UIKit
 
 public let screenSize = UIScreen.mainScreen().bounds
 
-class Gasto {
-    
-    var nome: String
-    var valor: Double
-    var data: String
-    var foto: UIImage?
-    var ehDinheiro: Bool = true
-    var categoria: CategoriaGasto!
-    
-    init(nome: String, valor: Double, data: String) {
-        
-        self.nome=nome
-        self.valor=valor
-        self.data=data
-        
-    }
-    
-}
-
 class Usuario {
-    
+
     var email: String
     var senha: String
     var nome: String
     var limiteMes: Double
-    var gasto: [Gasto]
-    
-    init(nome: String,email: String,senha: String,limiteMes: Double,gastos: [Gasto]) {
+    var cartoes = [Cartao]()
+    var gastos = [Gasto]()
+    var categoriasGastos = [String]()
+
+    init(nome: String,email: String,senha: String) {
         self.nome = nome
         self.email = email
         self.senha = senha
-        self.limiteMes = limiteMes
-        self.gasto = gastos
+        self.categoriasGastos.append("Outros")
     }
+
+    func addCategoriaGasto(categ: String) {
+        self.categoriasGastos.append(categ)
+    }
+
+    func getCategoriasGastos() -> [String] {
+        return self.categoriasGastos
+    }
+
+    func addGasto(gasto: Gasto) {
+        self.gastos.append(gasto)
+    }
+
+    func getGastos() -> [Gasto] {
+        return self.gastos
+    }
+
+    func addCartao(cartao: Cartao) {
+        self.cartoes.append(cartao)
+    }
+
+    func getCartoes() -> [Cartao] {
+        return self.cartoes
+    }
+
+    func setLimiteMes(limite: Double) {
+        self.limiteMes = limite
+    }
+
+    func getLimiteMes() -> Double {
+        return self.limiteMes
+    }
+}
+
+class Gasto {
     
+    var nome: String
+    var valor: Double
+    var data: NSDate
+    var foto: UIImage?
+    var ehDinheiro: Bool
+    var categoria: String!
+    var cartao: Cartao
+
+    init(nome: String, categoria: String, valor: Double, data: NSDate) {
+        self.nome = nome
+        self.categoria = categoria
+        self.valor = valor
+        self.data = data
+        self.ehDinheiro = true
+    }
+
+    init(nome: String, categoria: String, valor: Double, data: NSDate, cartao: Cartao) {
+        self.nome = nome
+        self.categoria = categoria
+        self.valor = valor
+        self.data = data
+        self.ehDinheiro = false
+        self.cartao = cartao
+    }
+
+    func setFoto(foto: UIImage) {
+        self.foto = foto
+    }
+
+    func getFoto() -> UIImage {
+        return self.foto
+    }
 }
 
 class Cartao {
@@ -52,23 +101,18 @@ class Cartao {
     var nome: String
     var limite: Double
     var cor: UIColor
-    
-    init(nome:String, limite:Double,cor: UIColor){
+
+    init(nome:String, limite:Double, cor: UIColor){
         self.nome = nome
         self.limite = limite
         self.cor = cor
-        
     }
-    
-}
 
-class CategoriaGasto {
-    
-    var nome: String
-    
-    init(nomeCat: String) {
-        self.nome = nomeCat
-        
+    func setLimite(limite: Double) {
+        self.limite = limite
     }
-    
+
+    func getLimite() -> Double {
+        return self.limite
+    }
 }
