a4c45084c28f0bd57ab87628044817404162ac58
andreparente <andremachadoparente@gmail.com>
2016-03-23 23:19:12 -0300

    Tabelas/Entidades e estrutura do HistoricoTabela

--- ControleDeGastos/HistoricoTabelaViewController.swift
+++ ControleDeGastos/HistoricoTabelaViewController.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-class HistoricoTabelaViewController: UIViewController {
+class HistoricoTabelaViewController: UIViewController,UITableViewDelegate, UITableViewDataSource  {
+    
+    var tableView: UITableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        let screenSize = UIScreen.mainScreen().bounds
+        tableView.frame = CGRectMake(screenSize.minX,screenSize.minY+22,screenSize.width,screenSize.height)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+
+    }
+    
+    //funçao que diz a quantidade de células
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+    }
+    
+    //funçao que seta as células
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        return cell
+            
+    }
+    
+    //funçao que é chamada ao clicar em determinada célula
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        print("You selected cell #\(indexPath.row)!")
+        
     }
 
     override func didReceiveMemoryWarning() {
