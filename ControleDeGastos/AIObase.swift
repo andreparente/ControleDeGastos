f4c4a01f57af2c4480258b6bf53242d8f03227a5
Caio Valente <caio-valente@hotmail.com>
2016-04-01 18:42:31 -0300

    funcoes de edicao de usuario

--- ControleDeGastos/AIObase.swift
+++ ControleDeGastos/AIObase.swift
@@ -74,14 +74,14 @@ public class AIO {
         limSuperior = usuarioLogado!.getCartoes().count
         limInferior = limSuperior - ramCartoesQtd - 1
         limInferior = (limInferior > 0) ? limInferior : limSuperior
-        print("inferior: \(limInferior), superior: \(limSuperior)")
         for (i=limInferior; i < limSuperior; i += 1) {
+            print("inferior: \(limInferior), superior: \(limSuperior)")
             salvarCartao(usuarioLogado!.getCartoes()[i], usuario: usuarioLogado!)
         }
         
         // salva os gastos que ainda nao foram salvos
         limSuperior = usuarioLogado!.getGastos().count
-        limInferior = limSuperior - ramCartoesQtd - 1
+        limInferior = limSuperior - ramGastosQtd - 1
         limInferior = (limInferior > 0) ? limInferior : limSuperior
         for (i=limInferior; i < limSuperior; i += 1) {
             salvarGasto(usuarioLogado!.getGastos()[i], usuario: usuarioLogado!)
@@ -90,6 +90,9 @@ public class AIO {
         // salva ultimo usuario
         salvarUltimoUsuario(usuarioLogado!)
         
+        // zera variaveis globais
+        ramCartoesQtd = 0
+        ramGastosQtd = 0
         usuarioLogado = nil
     }
     
@@ -120,13 +123,7 @@ public class AIO {
             //print ("Li do arquivo \(file) a string:\n\(text)")
             
             for var i in 0..<entries.count-1 {
-                let attributes = entries[i].componentsSeparatedByString(attributeSeparator)
-                let novoUsuario = Usuario(nome: attributes[0], email: attributes[1], senha: attributes[2])
-                // adiciona as categorias ao novo usuario
-                let categorias = attributes[3].componentsSeparatedByString(arraySeparator)
-                for categ in categorias {
-                    novoUsuario.addCategoriaGasto(categ)
-                }
+                let novoUsuario = stringToUsuario(entries[i])
                 // adiciona o novo usuario ao array de usuarios
                 ramUsuarios.append(novoUsuario)
             }
@@ -212,19 +209,9 @@ public class AIO {
             //print ("Li do arquivo \(file) a string:\n\(text)")
             
             for var i in 0..<entries.count-1 {
-                let attributes = entries[i].componentsSeparatedByString(attributeSeparator)
-                let numParametros = attributes.count
-                let novoGasto = Gasto(nome: attributes[0], categoria: attributes[1], valor: Int(attributes[2])!, data: attributes[3])
-                
-                let indUsuario = indiceUsuarioPorEmail(usuario.email)
-                if (numParametros == 5) { // quando possui cartao, adiciona o cartao
-                    let indCartao = ramUsuarios[indUsuario].getCartaoIndex(attributes[4])
-                    if (indCartao != -1) {
-                        novoGasto.cartao = ramUsuarios[indUsuario].cartoes[indCartao]
-                    }
-                }
-                
+                let novoGasto = stringToGasto(entries[i], usuario: usuario)
                 // adiciona o gasto ao seu respectivo usuario
+                let indUsuario = indiceUsuarioPorEmail(usuario.email)
                 ramUsuarios[indUsuario].addGasto(novoGasto)
             }
         
@@ -265,10 +252,7 @@ public class AIO {
         }
     }
     
-    // possiveis formatos:
-    // "nome \n categoria \n valor \n data \n nomeCartao"
-    // "nome \n categoria \n valor \n data "
-    func editarGasto (novoGasto: Gasto, usuario: Usuario) {
+    func editarGasto (gastoEditado: Gasto, usuario: Usuario) {
         let file = file_gastos
         let path = dir + "/" + file + "-" + usuario.email + "." + file_format
         
@@ -280,18 +264,17 @@ public class AIO {
             //print ("Li do arquivo \(file) a string:\n\(text)")
             
             for var i in 0..<entries.count-1 {
-                let attributes = entries[i].componentsSeparatedByString(attributeSeparator)
-                let gasto = Gasto(nome: attributes[0], categoria: attributes[1], valor: Int(attributes[2])!, data: attributes[3])
+                var gasto = stringToGasto(entries[i], usuario: usuario)
                 
                 // se for o objeto desejado, edita
-                if (novoGasto.nome == gasto.nome && novoGasto.data == gasto.data) {
-                    // edita no disco
-                    result += gastoToString(novoGasto)
+                if (gastoEditado.nome == gasto.nome && gastoEditado.data == gasto.data) {
+                    // substitui o objeto pelo editado
+                    gasto = gastoEditado
                     // edita na RAM
-                    usuarioLogado?.gastos[i] = novoGasto
-                } else {
-                    result += gastoToString(gasto)
+                    usuarioLogado?.gastos[i] = gasto
                 }
+                // substitui o objeto pelo editado
+                result += gastoToString(gasto)
             }
             try result.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding)
         } catch {
@@ -317,6 +300,9 @@ public class AIO {
     }
     
     // gera uma string que eh a descricao do gasto na base
+    // possiveis formatos:
+    // "nome \n categoria \n valor \n data \n nomeCartao"
+    // "nome \n categoria \n valor \n data "
     internal func gastoToString (gasto: Gasto) -> String {
         var newEntrie = "\(gasto.nome)\(attributeSeparator)"
         newEntrie += "\(gasto.categoria)\(attributeSeparator)"
@@ -336,7 +322,35 @@ public class AIO {
         newEntrie += "\(cartao.cor)\(objectSeparator)"
         return newEntrie
     }
-
+    
+    // pega um gasto e gera uma string pra guardar na base
+    internal func stringToGasto(str: String, usuario: Usuario) -> Gasto {
+        let attributes = str.componentsSeparatedByString(attributeSeparator)
+        let numParametros = attributes.count
+        let novoGasto = Gasto(nome: attributes[0], categoria: attributes[1], valor: Int(attributes[2])!, data: attributes[3])
+        
+        let indUsuario = indiceUsuarioPorEmail(usuario.email)
+        if (numParametros == 5) { // quando possui cartao, adiciona o cartao
+            let indCartao = ramUsuarios[indUsuario].getCartaoIndex(attributes[4])
+            if (indCartao != -1) {
+                novoGasto.cartao = ramUsuarios[indUsuario].cartoes[indCartao]
+            }
+        }
+        return novoGasto
+    }
+    
+    // pega um usuario e gera uma string pra guardar na base
+    internal func stringToUsuario(str: String) -> Usuario {
+        let attributes = str.componentsSeparatedByString(attributeSeparator)
+        let novoUsuario = Usuario(nome: attributes[0], email: attributes[1], senha: attributes[2])
+        // adiciona as categorias ao novo usuario
+        let categorias = attributes[3].componentsSeparatedByString(arraySeparator)
+        for categ in categorias {
+            novoUsuario.addCategoriaGasto(categ)
+        }
+        return novoUsuario
+    }
+    
     internal func adicionarUsuario (usuario: Usuario) {
         let file = file_usuarios
         let path = dir + "/" + file + "." + file_format
@@ -367,47 +381,34 @@ public class AIO {
         
     }
     
-    // ainda nao funciona
-    internal func editarUsuario (usuario: Usuario) {
+    internal func editarUsuario (usuarioEditado: Usuario) {
         let file = file_usuarios
         let path = dir + "/" + file + "." + file_format /* ./usuarios.aio */
         
-        var textoAntesDoSave = ""
-        
-        //reading
+        //reading and writing
         do {
-            textoAntesDoSave = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding) as String
+            let text = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+            let entries = text.componentsSeparatedByString(objectSeparator)
+            var result = ""
+            //print ("Li do arquivo \(file) a string:\n\(text)")
+            
+            for var i in 0..<entries.count-1 {
+                var usuario = stringToUsuario(entries[i])
+                
+                // se for o objeto desejado, edita
+                if (usuarioEditado.email == usuario.email) {
+                    // substitui o objeto pelo editado
+                    usuario = usuarioEditado
+                    // edita na RAM
+                    ramUsuarios[i] = usuario
+                }
+                // salva no disco
+                result += usuarioToString(usuario)
+            }
+            try result.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding)
         } catch {
             print ("erro na leitura do arquivo \(path)")
         }
-        
-        //writing
-        do {
-            // copia todo o texto anterior
-            var entries = textoAntesDoSave
-            
-            // adiciona a nova entrada
-            var newEntrie = "\(usuario.nome)\(attributeSeparator)"
-            newEntrie += "\(usuario.email)\(attributeSeparator)"
-            newEntrie += "\(usuario.senha)\(attributeSeparator)"
-            let categorias = usuario.getCategoriasGastos()
-            let nCateg = categorias.count
-            if (nCateg > 0) {
-                for i in 0..<(nCateg-1) {
-                    newEntrie += "\(categorias[i])\(arraySeparator)"
-                }
-                newEntrie += "\(categorias[nCateg-1])\(attributeSeparator)"
-            }
-            newEntrie += "\(usuario.email)\(objectSeparator)"
-            
-            entries += newEntrie
-            
-            try entries.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding)
-            print ("Adicionei ao arquivo \(file) a entrada:\n\(newEntrie)")
-        } catch {
-            print ("erro na escrita do arquivo \(file)")
-        }
-        
     }
     
     internal func carregarEmailUltimoUsuario () -> String {
