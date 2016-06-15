d758faa912b5d51ae26f3c75559966ed80f5fac6
Felipe Viberti <felipegus8@yahoo.com.br>
2016-06-14 23:55:30 -0300

    Testando se o user está conectado na internet

--- ControleDeGastos/LaunchViewController.swift
+++ ControleDeGastos/LaunchViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
-
+import SystemConfiguration
 class LaunchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if Reachability.isConnectedToNetwork() == false
+        {
+            let alert=UIAlertController(title:"iCloud não disponível", message: "Você nāo está conectado à internet", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title:"Ok",style: UIAlertActionStyle.Default,handler:{(action) -> Void in
+                exit(0)
+                }))
+            dispatch_async(dispatch_get_main_queue(),{
+                self.presentViewController(alert,animated: true, completion: nil)
+            })
+        }
+        else
+        {
         var dict1 = plist.getData()
         print(plist.plistName)
         if dict1 != nil && dict1!["isLogged"] as! String != "loggedOut" {
@@ -33,7 +45,7 @@ class LaunchViewController: UIViewController {
         }
 
     }
-    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -46,5 +58,22 @@ class LaunchViewController: UIViewController {
             let vc = segue.destinationViewController as! UITabBarController
             vc.selectedIndex = 1
         }
+    }
+}
+public class Reachability {
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
     }
 }
